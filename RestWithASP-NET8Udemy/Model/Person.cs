90a3881d5e388c0708678cfc0c10cb96d783825b
marliseborba <borba.marlise@gmail.com>
2025-03-28 03:11:26 -0300

    Create Person class

---
+++ RestWithASP-NET8Udemy/Model/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace RestWithASP_NET8Udemy.Model
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string gender { get; set; }
+    }
+}
