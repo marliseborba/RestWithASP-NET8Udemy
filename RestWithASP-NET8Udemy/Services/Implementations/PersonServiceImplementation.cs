593a6de261c7eda0fc9e3d786e7a9e105eb605c5
marliseborba <borba.marlise@gmail.com>
2025-04-11 13:19:19 -0300

    Implements IPersonService

---
+++ RestWithASP-NET8Udemy/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,63 @@
+﻿using RestWithASP_NET8Udemy.Model;
+
+namespace RestWithASP_NET8Udemy.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+            }
+            return persons;
+        }
+
+        public Person FindById(long id)
+        {
+            return new Person 
+            { 
+                Id = IncrementAndGet(),
+                FirstName = "Marlise",
+                LastName = "Marlise",
+                Address = "Ijuí - RS - Brasil",
+                Gender = "Female"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person Name " + i,
+                LastName = "Person LastName " + i,
+                Address = "Some Address",
+                Gender = "Female"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
