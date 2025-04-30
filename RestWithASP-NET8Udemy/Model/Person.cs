f9dbcacde1e07c59dedfeb750f35dca54d2c301d
marliseborba <borba.marlise@gmail.com>
2025-04-30 10:10:05 -0300

    Adding Annotations

--- RestWithASP-NET8Udemy/Model/Person.cs
+++ RestWithASP-NET8Udemy/Model/Person.cs
@@ -1,11 +1,19 @@
-﻿namespace RestWithASP_NET8Udemy.Model
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestWithASP_NET8Udemy.Model
 {
+[Table("person")]
     public class Person
     {
+        [Column("id")]
         public long Id { get; set; }
+        [Column("first_name")]
         public string FirstName { get; set; }
+        [Column("last_name")]
         public string LastName { get; set; }
+        [Column("address")]
         public string Address { get; set; }
+        [Column("gender")]
         public string Gender { get; set; }
     }
 }
