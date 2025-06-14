db18f97df8c804eeb5a4dba2272d3019c791ce80
marliseborba <borba.marlise@gmail.com>
2025-06-14 17:35:35 -0300

    Creating and mapping Books entity

---
+++ RestWithASP-NET8Udemy/Model/Books.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestWithASP_NET8Udemy.Model
+{
+    [Table("Books")]
+    public class Books
+    {
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("author")]
+        public string Author { get; set; }
+        [Column("launch_date")]
+        public DateTime LaunchDate { get; set; }
+        [Column("price")]
+        public double Price { get; set; }
+        [Column("title")]
+        public string Title { get; set; }
+
+
+    }
+}
