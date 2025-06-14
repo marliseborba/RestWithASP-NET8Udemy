7c90ff1b4b491b269b428d36d84a425887537a55
marliseborba <borba.marlise@gmail.com>
2025-06-14 17:48:13 -0300

    Creating Books Business

---
+++ RestWithASP-NET8Udemy/Business/Implementations/BooksBusinessImplementation.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using RestWithASP_NET8Udemy.Model;
+using RestWithASP_NET8Udemy.Model.Context;
+using RestWithASP_NET8Udemy.Repository;
+using System;
+
+namespace RestWithASP_NET8Udemy.Business.Implementations
+{
+    public class BooksBusinessImplementation : IBooksBusiness
+    {
+        private readonly IBooksRepository _repository;
+
+        public BooksBusinessImplementation(IBooksRepository repository)
+        {   
+            _repository = repository;
+        }
+        public List<Books> FindAll()
+        {
+            return _repository.FindAll();
+        }
+
+        public Books FindById(long id)
+        {
+            return _repository.FindById(id);
+        }
+
+        public Books Create(Books books)
+        {
+            return _repository.Create(books);
+        }
+        public Books Update(Books books)
+        {
+            return _repository.Update(books);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        } 
+    }
+}
