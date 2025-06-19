a59ac555292e2c2c15ba7e4211290fb84197e0e8
marliseborba <borba.marlise@gmail.com>
2025-06-19 16:11:17 -0300

    Creating Generic Repository

---
+++ RestWithASP-NET8Udemy/Repository/Generic/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using RestWithASP_NET8Udemy.Model;
+using RestWithASP_NET8Udemy.Model.Base;
+
+namespace RestWithASP_NET8Udemy.Repository
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        T Create (T item);
+        T FindById (long id);
+        List<T> FindAll();
+        T Update (T item);
+        void Delete (long id);
+        bool Exists(long id);
+    }
+}
