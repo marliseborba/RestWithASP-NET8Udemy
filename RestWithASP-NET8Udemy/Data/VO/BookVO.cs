9a4a14257f2823dc211f781ebd3e75546700c1da
marliseborba <borba.marlise@gmail.com>
2025-06-19 18:11:08 -0300

    Implementing BookConverter

---
+++ RestWithASP-NET8Udemy/Data/VO/BookVO.cs
@@ -0,0 +1,20 @@
+﻿using RestWithASP_NET8Udemy.Model.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestWithASP_NET8Udemy.Data.VO
+{
+    public class BookVO
+    {
+        public long Id { get; set; }
+
+        public string Author { get; set; }
+
+        public DateTime LaunchDate { get; set; }
+
+        public double Price { get; set; }
+
+        public string Title { get; set; }
+
+
+    }
+}
