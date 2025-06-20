8527602b1b1cefcaef6e5c9fc707ff3f8bdfd1bc
marliseborba <borba.marlise@gmail.com>
2025-06-20 19:48:47 -0300

    Creating Filters

---
+++ RestWithASP-NET8Udemy/Hypermedia/Filters/HyperMediaFilterOptions.cs
@@ -0,0 +1,9 @@
+﻿using RestWithASP_NET8Udemy.Hypermedia.Abstract;
+
+namespace RestWithASP_NET8Udemy.Hypermedia.Filters
+{
+    public class HyperMediaFilterOptions
+    {
+        public List<IResponseEnricher> ContentResponseEnricherList { get; set; } = new List<IResponseEnricher>();
+    }
+}
