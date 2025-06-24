9d6b178f3c8fdc78bc66281d311b89e7e00d57ca
marliseborba <borba.marlise@gmail.com>
2025-06-24 19:40:39 -0300

    Creating UserRepository and UserVO

---
+++ RestWithASP-NET8Udemy/Data/VO/UserVO.cs
@@ -0,0 +1,8 @@
+﻿namespace RestWithASP_NET8Udemy.Data.VO
+{
+    public class UserVO
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ RestWithASP-NET8Udemy/Repository/UserRepository.cs
@@ -0,0 +1,38 @@
+﻿using RestWithASP_NET8Udemy.Data.VO;
+using RestWithASP_NET8Udemy.Model;
+using RestWithASP_NET8Udemy.Model.Context;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RestWithASP_NET8Udemy.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly MySQLContext _context;
+
+        public UserRepository(MySQLContext context)
+        {
+            _context = context;
+        }
+
+        public User ValidadeCredentials(UserVO user)
+        {
+            var pass = ComputeHash(user.Password, SHA256.Create());
+            return _context.Users.FirstOrDefault(u => (u.UserName == user.Username) && u.Password == pass);
+        }
+
+        private object ComputeHash(string input, HashAlgorithm hashAlgorithm)
+        {
+            Byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+            Byte[] hashedBytes = hashAlgorithm.ComputeHash(inputBytes);
+            
+            var builder = new StringBuilder();
+
+            foreach ( var item in hashedBytes )
+            {
+                builder.Append(item.ToString("x2"));
+            }
+            return builder.ToString();
+        }
+    }
+}
