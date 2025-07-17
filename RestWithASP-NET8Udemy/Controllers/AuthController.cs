07f7a7ce6266d74dfa9c74dfee483a8ae4c2261b
marliseborba <borba.marlise@gmail.com>
2025-07-17 00:09:34 -0300

    Creating Auth Controller

---
+++ RestWithASP-NET8Udemy/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using RestWithASP_NET8Udemy.Business;
+using RestWithASP_NET8Udemy.Data.VO;
+
+namespace RestWithASP_NET8Udemy.Controllers
+{
+    [ApiVersion("1")]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private ILoginBusiness _loginBusiness;
+
+        public AuthController(ILoginBusiness loginBusiness)
+        {
+            _loginBusiness = loginBusiness;
+        }
+
+        [HttpPost]
+        [Route("signin")]
+        public IActionResult Signin([FromBody] UserVO user)
+        {
+            if (user == null)
+                return BadRequest("Invalid client request");
+            var token = _loginBusiness.ValidateCredentials(user);
+            if (token == null) return Unauthorized();
+            return Ok(token);
+        }
+    }
+}
