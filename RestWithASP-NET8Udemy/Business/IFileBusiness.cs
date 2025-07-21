ea209c23d4dc55ed493b92e7d523885bb55f35dc
marliseborba <borba.marlise@gmail.com>
2025-07-21 03:20:02 -0300

    Implementing the method responsible for saving files to disk

---
+++ RestWithASP-NET8Udemy/Business/IFileBusiness.cs
@@ -0,0 +1,11 @@
+﻿using RestWithASP_NET8Udemy.Data.VO;
+
+namespace RestWithASP_NET8Udemy.Business
+{
+    public interface IFileBusiness
+    {
+        public byte[] GetFile(string filename);
+        public Task<FileDetailVO> SaveFileToDisk(IFormFile file);
+        public Task<List<FileDetailVO>> SaveFilesToDisk(IList<IFormFile> file);
+    }
+}
---
+++ RestWithASP-NET8Udemy/Business/Implementations/FileBusinessImplementation.cs
@@ -0,0 +1,46 @@
+﻿using RestWithASP_NET8Udemy.Data.VO;
+
+namespace RestWithASP_NET8Udemy.Business.Implementations
+{
+    public class FileBusinessImplementation : IFileBusiness
+    {
+        private readonly string basePath;
+        private readonly IHttpContextAccessor _context;
+
+        public FileBusinessImplementation(IHttpContextAccessor context)
+        {
+            _context = context;
+            basePath = Directory.GetCurrentDirectory() + "\\UploadDir\\";
+        }
+
+        public byte[] GetFile(string filename)
+        {
+            throw new NotImplementedException();
+        }
+        public async Task<FileDetailVO> SaveFileToDisk(IFormFile file)
+        {
+            FileDetailVO fileDetail = new FileDetailVO();
+
+            var fileType = Path.GetExtension(file.FileName);
+            var baseUrl = _context.HttpContext.Request.Host;
+
+            if (fileType.ToLower() == ".pdf" || fileType.ToLower() == ".jpg" || fileType.ToLower() == ".png" || fileType.ToLower() == ".jpeg");
+            {
+                var docName = Path.GetFileName(file.FileName);
+                if (file != null && file.Length > 0)
+                {
+                    var destination = Path.Combine(baseUrl + "/api/file/v1/" + fileDetail.DocumentName);
+
+                    using var stream = new FileStream(destination, FileMode.Create);
+                    await file.CopyToAsync(stream);
+                }
+            }
+            return fileDetail;
+        }
+
+        public Task<List<FileDetailVO>> SaveFilesToDisk(IList<IFormFile> file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
